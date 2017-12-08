7aa098d35b436a32d0acfc8645445428b83b6c73
Volodymyr Padovsky <vld06@ukr.net>
2017-12-08 16:54:54 +0200

    usercart func added

--- main.js
+++ main.js
@@ -7,11 +7,78 @@ Cart.prototype.add = function (item) {
     this.goods.push(item);
 };
 
-/**
- * You code here
- * Item...
- * UserCart...
- */
+//You code here
+function UserCart() {};
+
+UserCart.prototype = Cart.prototype;
+
+UserCart.prototype.amount = function() {
+    let totalCost = 0;
+    this.goods.forEach(function(item) {
+        totalCost += item.price;
+    });
+    return totalCost;
+};
+
+
+UserCart.prototype.updateQnt = function(idNum, qnt) {
+
+    //get the quantity of specific product
+    let getQnt = function(idNum, goods) {
+        let count = 0;
+        for (let item in goods) {
+            if (goods[item].idNum === idNum) {
+                count++;
+            };
+        };
+        return count;
+    };
+
+    //get first object's index with the given ID
+    let getObjIndex = function(idNum, goods) {
+        for (let index in goods) {
+            if (goods[index].idNum === idNum) {
+                return index;
+            };
+        };
+    };
+
+    //get difference between the required quantity of goods and the current
+    let diff = qnt - getQnt(idNum, this.goods);
+    if (diff > 0) {
+        for (let i = 0; i < diff; i++) {
+            this.goods.push(Object.assign({}, this.goods[getObjIndex(idNum, this.goods)]))
+        };
+
+    } else {
+        for (let i = 0; i < -diff; i++) {
+            this.goods.splice(getObjIndex(idNum, this.goods), 1);
+        };
+    };
+};
+
+
+UserCart.prototype.remove = function(idNum) {
+    this.goods.forEach(function(item, i, goods) {
+        if (item.idNum === idNum) {
+            goods.splice(i, 1);
+        }
+    });
+};
+
+UserCart.prototype.clear = function() {
+    this.goods.length = 0;
+};
+
+UserCart.prototype.getAll = function() {
+    return this.goods;
+};
+
+function Item(idNum, title, price) {
+    this.idNum = idNum;
+    this.title = title;
+    this.price = price;
+};
 
 // Test
 const cart = new UserCart();
@@ -38,3 +105,4 @@ if (cart.getAll().length === 0) {
 } else {
     console.error('Clear error');
 }
+
